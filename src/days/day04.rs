aea3f1f71e46d90129018004c789828fc4a67ed3
Conny Sjögren <conny.sjogren@gmail.com>
2022-12-05 07:51:44 +0100

    day4

--- src/days/day04.rs
+++ src/days/day04.rs
@@ -1,9 +1,46 @@
+use std::ops::{RangeInclusive};
+
 use crate::{Solution, SolutionPair};
 
-pub fn solve() -> SolutionPair {
-    // Your solution here...
-    let sol1: u64 = 0;
-    let sol2: u64 = 0;
+fn parse_range(r: &str) -> RangeInclusive<i32> {
+    let (a, b) = r.split_once('-').unwrap();
+    let from = a.parse().unwrap();
+    let to = b.parse().unwrap();
+    from..=to
+}
 
-    (Solution::U64(sol1), Solution::U64(sol2))
+fn check_full_overlap(l: &RangeInclusive<i32>, r: &RangeInclusive<i32>) -> bool {
+    l.contains(r.start()) && l.contains(r.end()) 
+    || r.contains(l.start()) && r.contains(l.end())
+}
+
+fn check_any_overlap(l: &RangeInclusive<i32>, r: &RangeInclusive<i32>) -> bool {
+    l.contains(r.start()) || l.contains(r.end()) 
+    || r.contains(l.start()) || r.contains(l.end())
+}
+
+pub fn solve() -> SolutionPair {
+    let input = include_str!("../../input/day4/real.txt");
+
+    let sol1 = input
+        .lines()
+        .filter(|line| {
+            let (elf1, elf2) = line.split_once(',').unwrap();
+            let (r1, r2) = (parse_range(elf1), parse_range(elf2));
+            
+            check_full_overlap(&r1, &r2)
+        })
+        .count();
+
+    let sol2 = input
+        .lines()
+        .filter(|line| {
+            let (elf1, elf2) = line.split_once(',').unwrap();
+            let (r1, r2) = (parse_range(elf1), parse_range(elf2));
+            
+            check_any_overlap(&r1, &r2)
+        })
+        .count();
+
+    (Solution::USize(sol1), Solution::USize(sol2))
 }
