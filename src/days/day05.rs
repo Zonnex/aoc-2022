7132f975ca351765d1ce61ce0a0a63f3978fad39
Conny Sjögren <conny.sjogren@gmail.com>
2022-12-06 07:06:08 +0100

    day5

--- src/days/day05.rs
+++ src/days/day05.rs
@@ -1,9 +1,100 @@
+use std::collections::VecDeque;
+
 use crate::{Solution, SolutionPair};
 
-pub fn solve() -> SolutionPair {
-    // Your solution here...
-    let sol1: u64 = 0;
-    let sol2: u64 = 0;
+fn parse_state(state: &str) -> Vec<Vec<char>> {
+    let mut iterator = state.lines().rev();
+    let heading = iterator.next().expect("Failed to get header");
 
-    (Solution::U64(sol1), Solution::U64(sol2))
+    let stack_count = heading
+        .trim_end()
+        .chars()
+        .last()
+        .expect("Failed to get last")
+        .to_digit(10)
+        .expect("Wasn't a digit") as usize;
+
+    let mut stacks = vec![vec![]; stack_count];
+
+    for line in iterator {
+        for (stack, c) in line.chars().skip(1).step_by(4).enumerate() {
+            if c.is_alphabetic() {
+                stacks[stack].push(c);
+            }
+        }
+    }
+    stacks
+}
+
+#[derive(Debug)]
+struct Instruction {
+    amount: usize,
+    from: usize,
+    to: usize
+}
+
+fn part_one(mut state: Vec<Vec<char>>, instructions: &Vec<Instruction>) -> String {
+    for instruction in instructions {
+        for _ in 0..instruction.amount {
+            let v = state[instruction.from-1].pop().expect("No item to pop");
+            state[instruction.to-1].push(v);
+        }
+    }
+
+    let chars = state
+        .iter()
+        .filter_map(|stack| stack.last());
+
+    String::from_iter(chars)
+}
+
+fn part_two(mut state: Vec<Vec<char>>, instructions: &Vec<Instruction>) -> String {
+    let mut temp = VecDeque::new();
+    for instruction in instructions {
+        for _ in 0..instruction.amount {
+            let v = state[instruction.from-1].pop().expect("No item to pop");
+            temp.push_front(v);
+        }
+
+        while let Some(v) = temp.pop_front() {
+            state[instruction.to-1].push(v);
+
+        }
+    }
+
+    let chars = state
+        .iter()
+        .filter_map(|stack| stack.last());
+
+    String::from_iter(chars)
+}
+
+pub fn solve() -> SolutionPair {
+    let input = include_str!("../../input/day5/real.txt");
+
+    let (state, instructions) = input
+        .split_once("\r\n\r\n")
+        .expect("Invalid input for day5");
+
+    let state = parse_state(state);
+
+    let instructions = instructions
+        .lines()
+        .map(|l| {
+            let values = l.split_whitespace()
+                .filter_map(|s| s.parse::<usize>().ok())
+                .collect::<Vec<_>>();
+
+            Instruction {
+                amount: values[0],
+                from: values[1],
+                to: values[2]
+            }
+        })
+        .collect::<Vec<_>>();
+
+    let sol1 = part_one(state.clone(), &instructions);
+    let sol2 = part_two(state, &instructions);
+
+    (Solution::Str(sol1), Solution::Str(sol2))
 }
