75ee162bda17045243d826b4e2a9d57fe9fc24a0
Conny Sjögren <conny.sjogren@gmail.com>
2022-12-06 07:36:05 +0100

    day6

--- src/days/day06.rs
+++ src/days/day06.rs
@@ -1,9 +1,28 @@
+use std::collections::HashSet;
+
 use crate::{Solution, SolutionPair};
 
-pub fn solve() -> SolutionPair {
-    // Your solution here...
-    let sol1: u64 = 0;
-    let sol2: u64 = 0;
+fn find_first_distinct_combination(input: &str, size: usize) -> usize {
+    let mut hash_set = HashSet::new();
 
-    (Solution::U64(sol1), Solution::U64(sol2))
+    let (position, _) = input
+        .as_bytes()
+        .windows(size)
+        .enumerate()
+        .find(|(_, chars)| {
+            hash_set.clear();
+            chars.iter().all(|c| hash_set.insert(*c))
+        })
+        .expect("Expected to find a position");
+    position + size
+}
+
+
+pub fn solve() -> SolutionPair {
+    let input = include_str!("../../input/day6/real.txt");
+
+    let sol1: usize = find_first_distinct_combination(input, 4);
+    let sol2: usize = find_first_distinct_combination(input, 14);
+
+    (Solution::USize(sol1), Solution::USize(sol2))
 }
